cceed2b1860ed27a4186f933981bd3b3d91713fb
lvyuqing <lyq15360562781@163.com>
2019-12-11 17:55:49 +0800

    动态生成文章列表，与动态查询功能完成

--- admin/libs/https.js
+++ admin/libs/https.js
@@ -1,12 +1,14 @@
 //沙盒模式，ajax请求接口
 
-;(function(w){
+; (function (w) {
     let basics = 'http://localhost:8080/api/v1';
     site = {
-        basics : basics,//基地址
-        userLogin : basics + '/admin/user/login',//用户登录接口
-        userInfo : basics + '/admin/user/info',//获取用户信息
-    }   
+        basics: basics,//基地址
+        userLogin: basics + '/admin/user/login',//用户登录接口
+        userInfo: basics + '/admin/user/info',//获取用户信息
+        article_query: basics + '/admin/article/query',//文章搜索
+        category_list: basics + '/admin/category/list',//所有文章类别
+    }
     //1
     window.site = site;
 
