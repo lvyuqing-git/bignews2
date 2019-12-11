7b0a18acac860bcbf8ec0c5524d7675dc99f687a
lvyuqing <lyq15360562781@163.com>
2019-12-11 20:01:50 +0800

    删除功能完成

--- admin/libs/https.js
+++ admin/libs/https.js
@@ -8,6 +8,7 @@
         userInfo: basics + '/admin/user/info',//获取用户信息
         article_query: basics + '/admin/article/query',//文章搜索
         category_list: basics + '/admin/category/list',//所有文章类别
+        article_delete: basics + '/admin/article/delete',//删除文章
     }
     //1
     window.site = site;
